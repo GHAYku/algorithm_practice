dfb38537c94497e91058e566ed9d597f87754be1
GHAYku <kusa07122@gmail.com>
2021-11-07 10:53:34 +0000

    [Finish]約数

---
+++ pitagorasu.rb
@@ -0,0 +1,27 @@
+max = 1000
+
+class Pythagoras
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def array
+    [@a, @b, @c]
+  end
+end
+
+array = []
+
+(1..max).each do |a|
+  (1..a).each do |b|
+    (1..b).each do |c|
+      if a*a == b*b + c*c
+        array << Pythagoras.new(a, b, c)
+      end
+    end
+  end
+end
+
+p array.map{|x| x.array}
